fa7d37b086a3166f7acb5dd156de27902fe83fb6
Shahsawar Khan <shahsawarkh32@gmail.com>
2022-07-13 10:58:46 +0500

    intial Stage

---
+++ Multiply.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Multiply = () => {
+  return (
+    <View>
+      <Text>Multiply</Text>
+    </View>
+  )
+}
+
+export default Multiply
