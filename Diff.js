abf2e029ffc099f504f1292e6fc71603e2507690
Shahsawar Khan <shahsawarkh32@gmail.com>
2022-07-13 10:42:54 +0500

    adding Diff.js

---
+++ Diff.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Diff = () => {
+  return (
+    <View>
+      <Text>Diff</Text>
+    </View>
+  )
+}
+
+export default Diff
+
+const styles = StyleSheet.create({})
